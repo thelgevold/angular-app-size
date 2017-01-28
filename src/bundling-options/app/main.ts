4c7f7cb0ef27ca601d2fa881c9848cfadb93313c
Torgeir Helgevold <torgeirh@hotmail.com>
2017-01-28 11:21:06 -0500

    prodmode enabled

--- src/bundling-options/app/main.ts
+++ src/bundling-options/app/main.ts
@@ -1,3 +1,7 @@
 import { platformBrowser }    from '@angular/platform-browser';
 import { AppModuleNgFactory } from './aot/app.module.ngfactory';
+import { enableProdMode } from '@angular/core';
+
+enableProdMode();
+
 platformBrowser().bootstrapModuleFactory(AppModuleNgFactory);
